08176ee423cae1b23d8706146e1d6fa62a37efe2
Rafael-Batista-Dev <59888617+Rafael-Batista-Dev@users.noreply.github.com>
2020-02-22 16:51:57 -0300

    Subindo projeto

---
+++ models/User.js
@@ -0,0 +1,53 @@
+class User {
+  constructor(name, email, admin, birth, country, password, photo, gender) {
+    this._name = name;
+    this._gender = gender;
+    this._birth = birth;
+    this._email = email;
+    this._country = country;
+    this._admin = admin;
+    this._photo = photo;
+    this._password = password;
+    this._register = new Date();
+  }
+
+  get register() {
+    return this._register;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get gender() {
+    return this._gender;
+  }
+
+  get birth() {
+    return this._birth;
+  }
+
+  get email() {
+    return this._email;
+  }
+
+  get country() {
+    return this._country;
+  }
+
+  get admin() {
+    return this._admin;
+  }
+
+  get photo() {
+    return this._photo;
+  }
+
+  get password() {
+    return this._password;
+  }
+
+  set photo(value) {
+    this._photo = value;
+  }
+}
