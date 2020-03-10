014de05d4bcabc92f1152a17922348fc4bb367f2
Rafael-Batista-Dev <59888617+Rafael-Batista-Dev@users.noreply.github.com>
2020-03-10 20:39:01 -0300

    Adicionadoa propriedade btn-delete

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -44,7 +44,7 @@ class UserController {
           <td>${Utils.dateFormat(result._register)}</td>
           <td>
           <button type="button" class="btn btn-primary btn-edit btn-xs btn-flat">Editar</button>
-          <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+          <button type="button" class="btn btn-danger btn-delete btn-xs btn-flat">Excluir</button>
           </td>`;
 
           this.addEventsTr(tr);
