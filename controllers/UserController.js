1f9eb5f8c6d459852ba342c92e21d526ad76ea7a
Rafael-Batista-Dev <59888617+Rafael-Batista-Dev@users.noreply.github.com>
2020-03-11 08:03:56 -0300

    UserController.js Refatorado

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -33,22 +33,11 @@ class UserController {
             result._photo = content;
           }
 
-          tr.dataset.user = JSON.stringify(result);
+          let user = new User();
+          user.loadFromJSON(result);
 
-          tr.innerHTML = `
-          <td><img src="${
-            result._photo
-          }" alt="User Image" class="img-circle img-sm"></td>
-          <td>${result._name}</td>
-          <td>${result._email}</td>
-          <td>${result._admin ? "Sim" : "Não"}</td>
-          <td>${Utils.dateFormat(result._register)}</td>
-          <td>
-          <button type="button" class="btn btn-primary btn-edit btn-xs btn-flat">Editar</button>
-          <button type="button" class="btn btn-danger btn-delete btn-xs btn-flat">Excluir</button>
-          </td>`;
+          this.getTr(user, tr);
 
-          this.addEventsTr(tr);
           this.updateCount();
 
           this.formUpdateEl.reset();
@@ -180,8 +169,15 @@ class UserController {
   }
 
   addLine(dataUser) {
-    let tr = document.createElement("tr");
+    let tr = this.getTr(dataUser);
 
+    this.tableEl.appendChild(tr);
+
+    this.updateCount();
+  }
+
+  getTr(dataUser, tr = null) {
+    if (tr === null) tr = document.createElement("tr");
     tr.dataset.user = JSON.stringify(dataUser);
 
     tr.innerHTML = `
@@ -198,10 +194,7 @@ class UserController {
     </td>`;
 
     this.addEventsTr(tr);
-
-    this.tableEl.appendChild(tr);
-
-    this.updateCount();
+    return tr;
   }
 
   addEventsTr(tr) {
